d3591bf94bb59751650e4667cdaa0fb4d3e44c2b
Vladimir Zarypov <krre31@gmail.com>
2026-07-21 07:44:41 +0300

    Add reading WASM file to buffer

--- src/cli.zig
+++ src/cli.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const Stdout = @import("Stdout.zig");
 const build_options = @import("build_options");
 
-pub fn run(stdout: *Stdout, args: []const [:0]const u8) !void {
+pub fn run(io: std.Io, allocator: std.mem.Allocator, stdout: *Stdout, args: []const [:0]const u8) !void {
     if (args.len == 1) {
         try showHelp(stdout);
         return;
@@ -14,6 +14,8 @@ pub fn run(stdout: *Stdout, args: []const [:0]const u8) !void {
         try showVersion(stdout);
         return;
     }
+
+    try runWasm(io, allocator, arg_1, stdout);
 }
 
 fn showHelp(stdout: *Stdout) !void {
@@ -27,4 +29,21 @@ fn showHelp(stdout: *Stdout) !void {
 
 fn showVersion(stdout: *Stdout) !void {
     try stdout.writeAll(build_options.version);
+}
+
+fn runWasm(io: std.Io, allocator: std.mem.Allocator, file_name: [:0]const u8, stdout: *Stdout) !void {
+    try stdout.print("run file: {s}\n", .{file_name});
+
+    const cwd = std.Io.Dir.cwd();
+    const file = try cwd.openFile(io, file_name, .{ .mode = .read_only });
+    defer file.close(io);
+
+    const size = try file.length(io);
+    const buf = try allocator.alloc(u8, size);
+
+    var reader = file.reader(io, &.{});
+    try reader.interface.readSliceAll(buf);
+
+    std.debug.print("buffer len: {d}\n", .{buf.len});
+    std.debug.print("buffer content: {s}\n", .{buf});
 }
